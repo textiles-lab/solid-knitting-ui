fd1d250c7288f550d53d09d0f1c1085ee600af93
Mark Gillespie <mark.gillespie81@gmail.com>
2023-02-03 07:50:35 -0500

    Show spline direction on tubes
    TODO: figure out yarn direction

--- code/gm.mjs
+++ code/gm.mjs
@@ -581,6 +581,10 @@ export function length2(a) {
 	return dot(a,a);
 }
 
+export function dist(a, b) {
+	return length(sub(b, a));
+}
+
 export function svd(A_) {
 	if (!(A_.length === 9)) throw new Error("This svd code only works on 3x3 matricies.");
 
